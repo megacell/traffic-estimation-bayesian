f533705e6dcd58ecb378b0d91a1c581dbe8b668c
Cathy Wu <cathywu@alum.mit.edu>
2014-05-09 22:13:14 -0700

    refactoring, more data files

--- grid_simulation.py
+++ grid_simulation.py
@@ -28,6 +28,18 @@ def sample(A,iters=100,logp=[],errors_b=[],errors_x=[]):
         errors_x.append(error_x)
         if i % 50 == 0:
             print i
+
+    x_ans = getX(A)
+    print [(x,A.stats()[x]['mean']) for x in A.stats()]
+
+    error_b = np.linalg.norm(model.A.dot(np.array(x_ans)) - model.b_obs[:,0])
+    print "norm(Ax-b): %s" % error_b
+    print np.vstack((model.A.dot(np.array(x_ans)),model.b_obs[:,0]))
+
+    error_x = np.linalg.norm(model.x_true[:,0]-np.array(x_ans))
+    print "norm(x-x*): %s" % error_x
+    print np.vstack((np.array(x_ans),model.x_true[:,0]))
+ 
     return A, logp, errors_b, errors_x
 
 def plot(logp, errors_b, errors_x):
@@ -48,43 +60,32 @@ def plot(logp, errors_b, errors_x):
     plt.ylabel('norm(x-x*)')
     plt.xlabel('Sample')
     plt.show()
-
-def save(model,logp,errors_b,errors_x):
-    with open('%s_meta.pickle' % model.fname,'wb') as f:
+'%s_meta.pickle' % model.fname
+def save(fmetaname,logp,errors_b,errors_x):
+    with open(fmetaname,'wb') as f:
         pickle.dump((logp,errors_b,errors_x), f)
 
 if __name__ == "__main__":
+    fname = '%s.pickle' % model.fname
+    fmetaname = '%s_meta.pickle' % model.fname
     try:
         # load previous simulation
-        db = pymc.database.pickle.load('%s.pickle' % model.fname)
+        db = pymc.database.pickle.load(fname)
         A = MCMC(model, db=db)
-        with open('%s_meta.pickle' % model.fname,'r') as f:
+        with open(fmetaname,'r') as f:
             logp, errors_b, errors_x = pickle.load(f)
     except IOError:
         # run new simulation
-        A = MCMC(model, db='pickle', dbname='%s.pickle' % model.fname)
+        A = MCMC(model, db='pickle', dbname=fname)
         logp = []
         errors_b = []
         errors_x = []
         A.sample(iter=100)
-        A, logp, errors_b, errors_x = sample(A,iters=1000,logp=logp,\
-                errors_b=errors_b,errors_x=errors_x)
-        save(model,logp,errors_b,errors_x)
     # A.sample(iter=50000)
     # plot(A,suffix='-grid')
  
-    x_ans = getX(A)
-    print [(x,A.stats()[x]['mean']) for x in A.stats()]
-
-    error_b = np.linalg.norm(model.A.dot(np.array(x_ans)) - model.b_obs[:,0])
-    print "norm(Ax-b): %s" % error_b
-    print np.vstack((model.A.dot(np.array(x_ans)),model.b_obs[:,0]))
-
-    error_x = np.linalg.norm(model.x_true[:,0]-np.array(x_ans))
-    print "norm(x-x*): %s" % error_x
-    print np.vstack((np.array(x_ans),model.x_true[:,0]))
- 
-    A, logp, errors_b, errors_x = sample(A,iters=1000,logp=logp,\
+    A, logp, errors_b, errors_x = sample(A,iters=300,logp=logp,\
             errors_b=errors_b,errors_x=errors_x)
-    save(model,logp,errors_b,errors_x)
+    save(fmetaname,logp,errors_b,errors_x)
+
     plot(logp, errors_b, errors_x)
